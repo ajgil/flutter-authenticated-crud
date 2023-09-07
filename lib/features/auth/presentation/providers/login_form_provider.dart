dd703882cbc0f9a6bf40fee2e2c63fbf6a785283
ajgil <antoniojesusgil@gmail.com>
2023-09-07 09:36:36 +0200

    connected riverpod with login form

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -3,8 +3,56 @@ import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 // como crear el provider
 
-// 1 - State del provider
 
+// 3 - StateNotifierProvider -> el consumidor desde fuera
+// autoDispose -> limpia el formulario una vez llamado
+final loginFormProvider =
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
+
+
+// 2 - Como implementamos un notifier (statenotifier)
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  // en el constructor siempre se llama la construcción del estado inicial
+  LoginFormNotifier() : super(LoginFormState());
+
+  // metodos
+  onEmailChange(String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+        email: newEmail, isValid: Formz.validate([newEmail, state.password]));
+  }
+
+  onPasswordChange(String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+        password: newPassword,
+        isValid: Formz.validate([newPassword, state.email]));
+  }
+
+  onFormSubmit() {
+    //todo -> algo
+    _touchEveryField();
+    if (!state.isValid) return;
+
+    print(state);
+  }
+
+  _touchEveryField() {
+    // en su estado inicial son pure y hay que tocar cada campo
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+        isFormPosted: true,
+        email: email,
+        password: password,
+        isValid: Formz.validate([email, password]));
+  }
+}
+
+// 1 - State del provider
 class LoginFormState {
   // como queremos que luzca el
   final bool isPosting;
@@ -48,50 +96,3 @@ class LoginFormState {
   }
 }
 
-// 2 - Como implementamos un notifier (statenotifier)
-
-class LoginFormNotifier extends StateNotifier<LoginFormState> {
-  // en el constructor siempre se llama la construcción del estado inicial
-  LoginFormNotifier() : super(LoginFormState());
-
-  // metodos
-  onEmailChange(String value) {
-    final newEmail = Email.dirty(value);
-    state = state.copyWith(
-        email: newEmail, isValid: Formz.validate([newEmail, state.password]));
-  }
-
-  onPasswordChange(String value) {
-    final newPassword = Password.dirty(value);
-    state = state.copyWith(
-        password: newPassword,
-        isValid: Formz.validate([newPassword, state.email]));
-  }
-
-  onFormSubmit() {
-    //todo -> algo
-    _touchEveryField();
-    if (!state.isValid) return;
-
-    print(state);
-  }
-
-  _touchEveryField() {
-    // en su estado inicial son pure y hay que tocar cada campo
-    final email = Email.dirty(state.email.value);
-    final password = Password.dirty(state.password.value);
-
-    state = state.copyWith(
-        isFormPosted: true,
-        email: email,
-        password: password,
-        isValid: Formz.validate([email, password]));
-  }
-}
-
-// 3 - StateNotifierProvider -> el consumidor desde fuera
-// autoDispose -> limpia el formulario una vez llamado
-final loginFormProvider =
-    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
-  return LoginFormNotifier();
-});
