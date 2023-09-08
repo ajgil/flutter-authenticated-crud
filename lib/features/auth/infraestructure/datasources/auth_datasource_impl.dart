4bf2f13eadddaee8a7512fb3f70e376dedcf5585
ajgil <antoniojesusgil@gmail.com>
2023-09-08 09:54:05 +0200

    implementing auth login and domain pattern step 1

---
+++ lib/features/auth/infraestructure/datasources/auth_datasource_impl.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/constants/environment.dart';
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infraestructure/infrastructure.dart';
+import 'package:teslo_shop/features/auth/infraestructure/mappers/user_mapper.dart';
+
+// patter adapter para envolver dio y si queremos posteriormente cambiarlo por otra clase
+
+class AuthDatasourceImpl extends AuthDataSource {
+  final dio = Dio(BaseOptions(
+    baseUrl: Environment.apiUrl,
+  ));
+
+  @override
+  Future<User> checkAuthStatus(String token) {
+    // TODO: implement checkAuthStatus
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> login(String email, String password) async {
+    try{
+      final response = await dio.post('/auth/login', data: {
+        'email': email,
+        'password': password
+      });
+
+      final user = UserMapper.userJsonToEntity(response.data); //mapper
+      return user;
+
+    } catch (e) {
+        throw WrongCredentials();
+    }
+  }
+
+  @override
+  Future<User> register(String email, String password, String fullName) {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+}
